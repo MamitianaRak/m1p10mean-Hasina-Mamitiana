a1fddc432a6b84c35433b0cf68a76b83f2e9444d
MamyGhost <80816758+MamyGhost@users.noreply.github.com>
2023-01-30 21:41:44 +0300

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -15,7 +15,7 @@ app.use(
   cookieSession({
     name: process.env.COOKIE_NAME,
     secret: process.env.COOKIE_SECRET,
-    domain:'vercel.app',
+    domain:'.vercel.app',
     sameSite:'none',
     httpOnly: true,
     secureProxy:true
